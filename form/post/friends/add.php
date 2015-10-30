3579ab7dcad2c08f12ebe72e3107ece89187a178
Peter Sall <Peter@Peters-MBP.lan>
2015-10-30 17:04:59 +0100

    Check if user already is friend

--- form/post/friends/add.php
+++ form/post/friends/add.php
@@ -17,15 +17,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	}
 
 	if (!empty($friends)) {
-		$users_exists = sqlSelect("SELECT user_id, username FROM `users` WHERE user_id IN ({$friends}) OR username IN ('{$friends}');");
+		$users = sqlSelect("SELECT user_id, username FROM `users` WHERE user_id IN ({$friends}) OR username IN ({$friends});");
 
-		if (!$users_exists) {
+		if (!$users) {
 			if (strlen($friends) >= 3)
 				array_push($_SESSION['errors'], "<span class=\"ion-android-warning\"> Spelarna finns inte");
 			if (strlen($friends) == 1)
 				array_push($_SESSION['errors'], "<span class=\"ion-android-warning\"> Spelaren finns inte");
 		}
+
+		else {
+			$already_friends = sqlSelect("SELECT users.user_id, users.username, friends.status, friends.sender FROM users INNER JOIN `friends` ON users.user_id = friends.user_id WHERE friends.user_id IN ({$friends}) UNION SELECT users.user_id, users.username, friends.status, friends.sender FROM users INNER JOIN `friends` ON users.user_id = friends.friend_user_id WHERE friends.friend_user_id IN ({$friends});");
+
+			if ($already_friends) {
+				foreach ($already_friends as $friend) {
+					if ($friend['status'] == 1)
+						array_push($_SESSION['errors'], "<span class=\"ion-android-warning\"> Du är redan vän med <a href=\"profile?view={$friend['user_id']}\">{$friend['username']}</a>");
+					if ($friend['status'] == 0 && $friend['sender'] == $_SESSION['user']['id'])
+						array_push($_SESSION['errors'], "<span class=\"ion-android-warning\"> Du har redan skickat vänförfrågan till <a href=\"profile?view={$friend['user_id']}\">{$friend['username']}</a>");
+					if ($friend['status'] == 0 && $friend['sender'] != $_SESSION['user']['id'])
+						array_push($_SESSION['errors'], "<span class=\"ion-android-warning\"><a href=\"profile?view={$friend['user_id']}\">{$friend['username']}</a> har redan skickat vänförfrågan till dig");
+				}
+			}
+		}
 	}
+
+	if ($_SESSION['errors'])
+		header('Location: ../../../profile?view=friends');
 
 	if (!$_SESSION['errors']) {
 		require '../../../lib/Firebase/url.php';
@@ -42,16 +60,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			'unread' => 'true'
 		);
 
-		$friend_request = "INSERT INTO friends (user_id, friend_user_id, status, date) VALUES ";
+		$friend_request = "INSERT INTO friends (user_id, friend_user_id, status, sender, date) VALUES ";
 
-		foreach ($users_exists as $friend) {
-			$friend_request .= "({$_SESSION['user']['id']}, {$friend['user_id']}, 0, now()), ";
+		foreach ($users as $friend) {
+			$friend_request .= "({$_SESSION['user']['id']}, {$friend['user_id']}, 0, {$_SESSION['user']['id']}, now()), ";
 		}
 
 		$friend_request = rtrim($friend_request, ', ');
 		$friend_request .= ';';
 
-		foreach ($users_exists as $new_friend) {
+		foreach ($users as $new_friend) {
 			$firebase->push(usersNewsFeed($new_friend['user_id']), $firebaseArray);
 		}
 

4824c53519a035fb0c20ad4472ff44874e4f0167
Peter Sall <Peter@Peters-MBP.lan>
2016-08-19 15:24:03 +0200

    Remove extra SELECT

--- form/post/story/join.php
+++ form/post/story/join.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$story = $_POST['story'];
 
 		$me = sqlSelect("SELECT user_id FROM story_writers WHERE story_id = {$story} and user_id = {$_SESSION['me']['id']};");
-		$writers = sqlSelect("SELECT max_writers, (SELECT COUNT(story_writers.user_id) AS num_of_writers, status FROM story INNER JOIN story_writers ON story.story_id = story_writers.story_id WHERE story.story_id = {$story};");	
+		$writers = sqlSelect("SELECT max_writers, COUNT(story_writers.user_id) AS num_of_writers, status FROM story INNER JOIN story_writers ON story.story_id = story_writers.story_id WHERE story.story_id = {$story};");	
 
 		if (empty($me) && $writers[0]['status'] == 0 && $writers[0]['max_writers'] > $writers[0]['num_of_writers']) {
 			$joinStory = null;
